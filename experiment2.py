63da189540c456fdcc1667f6743a542ab2b8f7c0
diwakar chaudhary <boyroyal4853@gmail.com>
2026-09-12 23:57:39 +0530

    add

--- experiment2.py
+++ experiment2.py
@@ -15,9 +15,11 @@ def is_prime(n):
         i += 6
     return True
 
+
 # Test the function
 num = int(input("Enter a number: "))
 if is_prime(num):
     print(f"{num} is a prime number.")
 else:
     print(f"{num} is not a prime number.")
+    

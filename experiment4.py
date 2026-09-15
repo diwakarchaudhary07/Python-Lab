cb39101ea0a505c5ece3537201aa79e180a22ec9
diwakar chaudhary <boyroyal4853@gmail.com>
2026-09-15 23:58:44 +0530

    add

--- experiment4.py
+++ experiment4.py
@@ -4,8 +4,10 @@ n = int(input("Enter number of terms (n): "))
 
 sum_series = 0
 for i in range(1, n + 1):
+
     if i % 2 == 0:
         sum_series -= i  # Even terms are negative
+        
     else:
         sum_series += i  # Odd terms are positive
 
